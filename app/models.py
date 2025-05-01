611b3583b2a1fb878ed3040d29f2823e24d146a1
BreannaThelwell <anna.thelwell@hotmail.com>
2025-04-30 23:28:10 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(100), nullable=False)  
     email = db.Column(db.String(120), unique=True, nullable=False)  
     photo = db.Column(db.String(200))  
