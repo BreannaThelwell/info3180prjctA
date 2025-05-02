1f3cc24322e1d85a195d09cde4cb8174c92f2da6
Breanna Thelwell <anna.thelwell@hotmail.com>
2025-05-02 10:02:20 -0500

    seeder

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,11 @@
 #being used to avoid circular import
 
 from flask_sqlalchemy import SQLAlchemy #db import
+from flask_seeder import FlaskSeeder #seeder import
 from flask_jwt_extended import JWTManager #token import
 from flask_migrate import Migrate #db import
 
 db = SQLAlchemy()
 jwt = JWTManager()
 migrate = Migrate()
+seeder = FlaskSeeder()
