d2ed991585fc7a7a504d71e8ec4b45036dcb82da
Jessica Anderson <145405675+JessPAnd@users.noreply.github.com>
2025-05-03 02:13:46 -0500

    Add files via upload

---
+++ src/views/Logout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+authStore.logout();
+router.push('/login');
+</script>
